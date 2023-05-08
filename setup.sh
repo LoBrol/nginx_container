2c920f1f38d2860d02f101480a80026a26be86db
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-05-08 20:33:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,14 +11,14 @@ nala install zsh -y
 nala install neofetch -y
 
 
-nano /etc/nanorc
+# nano /etc/nanorc
 # --> edit file nanorc
 
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-nano /home/ubuntu/.zshrc
+# nano /home/ubuntu/.zshrc
 # --> edit file .zshrc
 
 
