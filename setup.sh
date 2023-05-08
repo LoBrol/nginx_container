f458c7e83c73a1c71cdb9461f360be10450ab9ff
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-05-08 21:12:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -32,7 +32,7 @@ wget https://raw.githubusercontent.com/LoBrol/nginx_container/main/nanorc
 cd /root
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-em .zshrc
+rm .zshrc
 wget https://raw.githubusercontent.com/LoBrol/nginx_container/main/.zshrc
 chsh -s $(which zsh)
 
