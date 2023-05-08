cd321f209b35154b7e699e9414558512cacb227e
LoBrol <67478509+LoBrol@users.noreply.github.com>
2023-05-08 23:40:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ dpkg-reconfigure --frontend noninteractive tzdata
 apt install nala -y
 nala upgrade -y
 nala install nano -y
+# nala install wget
 nala install curl -y
 nala install git -y
 nala install zsh -y
@@ -24,7 +25,7 @@ nala install neofetch -y
 # ----- SETUP NANO -----
 cd /etc
 rm nanorc
-wget https://raw.githubusercontent.com/LoBrol/nginx_container/main/nanorc
+wget https://raw.githubusercontent.com/LoBrol/nginx_container/main/file_to_be_copied/nanorc
 
 
 # ----- SETUP OH-MY-ZSH -----
@@ -32,7 +33,7 @@ cd /root
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm .zshrc
-wget https://raw.githubusercontent.com/LoBrol/nginx_container/main/.zshrc
+wget https://raw.githubusercontent.com/LoBrol/nginx_container/main/file_to_be_copied/.zshrc
 chsh -s $(which zsh)
 
 
@@ -42,7 +43,18 @@ nala install php -y
 nala install php-fpm -y
 a2enmod proxy_fcgi setenvif
 a2enconf php8.1-fpm
-
+cd /etc/nginx/sites-available
+rm default
+wget https://raw.githubusercontent.com/LoBrol/nginx_container/main/file_to_be_copied/default
+cd /etc/php/8.1/fpm/pool.d
+rm www.conf
+wget https://raw.githubusercontent.com/LoBrol/nginx_container/main/file_to_be_copied/www.conf
+service php8.1-fpm restart
+service nginx restart
+# service nginx configtest 
+cd /var/www/html
+rm -rf *
 
 # ----- DONE -----
+cd /
 history -cw && clear && neofetch
